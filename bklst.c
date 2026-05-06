1b93a927f697ac8254856999650d981526d9d715
Lawrence Lipenga <lawrencelipenga2020@gmail.com>
2026-05-06 04:13:59 +0200

    CORE: Proceed to Real-Time

--- bklst.c
+++ bklst.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 struct s_book {
  int pages;
@@ -7,7 +9,43 @@ struct s_book {
 }; 
 typedef struct s_book book;
 
+book *head = NULL;
+
+void mkbk(char *title, int pages) {
+ book *newbk, *tmpbk;
+ while (!head) { 
+ tmpbk = malloc(sizeof(book));
+ memset(tmpbk, 0, sizeof(book));
+ tmpbk->pages = pages;
+ strcpy(tmpbk->title, title);
+ tmpbk->title[63] = '\0';
+ tmpbk->next = 0;
+ newbk = tmpbk;
+ head = newbk;
+ }
+
+ for (tmpbk=head; tmpbk; tmpbk=tmpbk->next);
+ 
+ tmpbk = malloc(sizeof(book));
+ memset(tmpbk, 0, sizeof(book));
+ tmpbk->pages = pages;
+ strcpy(tmpbk->title, title);
+ tmpbk->title[63] = '\0';
+ tmpbk->next = NULL;
+ newbk = tmpbk;
+}
+
+void lsbk(char *searchstr) {
+ book *tmpbk;
+ for (tmpbk=head; tmpbk; tmpbk=tmpbk->next)
+  if ( !searchstr || strcmp(searchstr, tmpbk->title) == 0)
+   printf("%s %d\n", tmpbk->title, tmpbk->pages); 
+}
+
 int main () {
- printf("Hello, World!");
+ mkbk("The Fault in Our Stars", 367);
+ printf("New Book Inserted...\n");
+ 
+ lsbk(0); 
  return 0;
 }
